c1e2dcc19098165ab673aef42ed076368f32d580
bgebelein <bent.gebelein@akquinet.de>
2023-08-24 16:25:36 +0200

    Added PWA install popup for android

--- app/script.js
+++ app/script.js
@@ -525,11 +525,37 @@ function triggerToast(title, description, type = 'info') {
     }, 3500);
 };
 
-/* -------------------- Trigger iOS PWA install pop-up  -------------------- */
+/* -------------------- Trigger PWA install pop-ups  -------------------- */
 
-setTimeout(function () {
-    document.querySelector('#ios-pwa-popup').remove();
-}, 6000);
+// User Agent is Android
+if (navigator.userAgent.match(/Android/i)) {
+    // Select template
+    const androidPopupTemplate = document.querySelector('#android-popup-template');
+    const androidPopup = androidPopupTemplate.cloneNode(true).content.querySelector('#android-pwa-popup');
+
+    // Add pop-up to body
+    document.querySelector('body').appendChild(androidPopup, true);
+
+    // Remove pop-up after 6 sec
+    setTimeout(function () {
+        document.querySelector('#android-pwa-popup').remove();
+    }, 6000);
+}
+
+// User Agent is iPhone, iPad or iPod
+if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+    // Select template
+    const iosPopupTemplate = document.querySelector('#ios-popup-template');
+    const iosPopup = iosPopupTemplate.cloneNode(true).content.querySelector('#ios-pwa-popup');
+
+    // Add pop-up to body
+    document.querySelector('body').appendChild(iosPopup, true);
+
+    // Remove pop-up after 6 sec
+    setTimeout(function () {
+        document.querySelector('#ios-pwa-popup').remove();
+    }, 6000);
+}
 
 /* -------------------- Helpers -------------------- */
 
